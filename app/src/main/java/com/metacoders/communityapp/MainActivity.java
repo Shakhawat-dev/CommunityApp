7e1fb5df58e02feb9cb1354dca092230bb4ab920
rahat14 <rahatshovo6666@gmail.com>
2020-07-20 23:05:05 +0600

    Reformated Code

--- app/src/main/java/com/metacoders/communityapp/MainActivity.java
+++ app/src/main/java/com/metacoders/communityapp/MainActivity.java
@@ -3,12 +3,40 @@ package com.metacoders.communityapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button  videoBtn , audioBtn ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        videoBtn = findViewById(R.id.videoBtn);
+        audioBtn = findViewById(R.id.audioBtn) ;
+
+        videoBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+
+            }
+        });
+
+
+        audioBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+
+            }
+        });
+
+
+
     }
 }
