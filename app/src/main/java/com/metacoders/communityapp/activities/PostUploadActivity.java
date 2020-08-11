9afb8c2b3f0ba19ba038d24b9d3908daafd1882a
shaheensms <shaheensms111@gmail.com>
2020-08-12 02:11:13 +0600

    Upload Activity designed

---
+++ app/src/main/java/com/metacoders/communityapp/activities/PostUploadActivity.java
@@ -0,0 +1,16 @@
+package com.metacoders.communityapp.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.metacoders.communityapp.R;
+
+public class PostUploadActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post_upload);
+    }
+}
