664201ff74c2267e8df41567f3cb00afedb19f94
rahat14 <rahatshovo6666@gmail.com>
2020-09-11 15:43:03 +0600

    added  fb  and gmail .

--- app/src/main/java/com/metacoders/communityapp/activities/RegistrationActivity.java
+++ app/src/main/java/com/metacoders/communityapp/activities/RegistrationActivity.java
@@ -14,6 +14,13 @@ import android.widget.CheckBox;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
 import com.metacoders.communityapp.R;
 import com.metacoders.communityapp.api.NewsRmeApi;
@@ -35,17 +42,32 @@ import retrofit2.Response;
 
 public class RegistrationActivity extends AppCompatActivity {
 
+    private static final int RC_SIGN_IN = 1000;
     private TextInputEditText mName, mUserName, mEmail, mPassword;
     private Button mSignUpBtn;
-    CheckBox isCehcked ;
-
+    CheckBox isCehcked;
+    Button googleBtn ;
+    GoogleSignInOptions gso ;
+    GoogleSignInClient mGoogleSignInClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
-        isCehcked = findViewById(R.id.termsCheck) ;
+        isCehcked = findViewById(R.id.termsCheck);
+// google sign in builder
+        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+               .requestServerAuthCode("377758534352-4onskrq3rvnr7d8m3uekddh1hb5n178q.apps.googleusercontent.com")
+                .requestEmail()
+                .build();
 
+        /*
+        *if account is null then not yet signed in ;
+        *
+        */
+       //GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
         getSupportActionBar().hide();
         initializations();
 
@@ -53,16 +75,27 @@ public class RegistrationActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                if(isCehcked.isChecked())
-                {
+                if (isCehcked.isChecked()) {
                     register();
-                }
-                else {
-                    Toast.makeText(getApplicationContext() , "Please Agree To ur Terms & Conditions" , Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Please Agree To ur Terms & Conditions", Toast.LENGTH_SHORT).show();
                 }
 
             }
         });
+        googleBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                googleSignIn();
+            }
+        });
+
+
+    }
+
+    private void googleSignIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
     }
 
     private void register() {
@@ -76,14 +109,9 @@ public class RegistrationActivity extends AppCompatActivity {
             email = mEmail.getText().toString().trim();
             password = mPassword.getText().toString().trim();
 
-//            Call<RegistrationResponse> call = RetrofitClient
-//                    .getInstance()
-//                    .getApi()
-//                    .registration(name, userName, email, password);
+            NewsRmeApi api = ServiceGenerator.createService(NewsRmeApi.class, "00");
 
-            NewsRmeApi api  = ServiceGenerator.createService(NewsRmeApi.class , "00") ;
-
-            Call<RegistrationResponse> call = api.registration(name, userName, email, password) ;
+            Call<RegistrationResponse> call = api.registration(name, userName, email, password);
 
             call.enqueue(new Callback<RegistrationResponse>() {
                 @Override
@@ -107,7 +135,7 @@ public class RegistrationActivity extends AppCompatActivity {
                 @Override
                 public void onFailure(Call<RegistrationResponse> call, Throwable t) {
                     Log.d(Constants.TAG, "onResponse: register" + t.toString());
-                    Toast.makeText(RegistrationActivity.this,"" + t.toString() , Toast.LENGTH_LONG).show();
+                    Toast.makeText(RegistrationActivity.this, "" + t.toString(), Toast.LENGTH_LONG).show();
                 }
             });
         }
@@ -119,5 +147,35 @@ public class RegistrationActivity extends AppCompatActivity {
         mEmail = (TextInputEditText) findViewById(R.id.regi_email);
         mPassword = (TextInputEditText) findViewById(R.id.regi_pass);
         mSignUpBtn = (Button) findViewById(R.id.signUPBtn);
+        googleBtn = findViewById(R.id.gButton) ;
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+
+            // Signed in successfully, show authenticated UI.
+            Log.d("TAG", "handleSignInResult: "+ account.getDisplayName()
+            + account.getId() + " " + account.getIdToken());
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.w("TAG", "signInResult:failed code=" + e.getStatusCode());
+
+        }
     }
 }
